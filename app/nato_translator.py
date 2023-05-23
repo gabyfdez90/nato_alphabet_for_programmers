30cdc72859741d998469d6db2eaea4dc343bcd62
Gabriela M. Fernández <galalou2010@gmail.com>
2023-05-23 19:26:18 +0200

    added error handling with new function

--- app/nato_translator.py
+++ app/nato_translator.py
@@ -1,5 +1,16 @@
 from app.data import alphabet
 
+def generate_word(dictionary, word):
+    """ Auxiliary function to translate_word(), that handles exceptions."""
+
+    try:
+          output_list = [dictionary[letter] for letter in word]
+    except KeyError:
+        print("Sorry, only letters in the alphabet, please.")
+        generate_word()
+    return output_list
+
+
 def translate_word(input_word): 
     """ Accepts any word as input and returns a list that represents its translation into Programmers NATO Alphabet."""
     
@@ -13,6 +24,7 @@ def translate_word(input_word):
 
     #Create a list of the code words necessary to complete an user input
     input_word = input_word.upper()
-    output = [nato_dictionary[letter] for letter in input_word]
 
-    return output
+    output_list = generate_word(nato_dictionary, input_word)
+
+    return output_list
